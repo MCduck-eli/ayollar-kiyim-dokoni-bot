5356cd2c70b4009f0029cce9ac3dc1ce7124c3de
Eldor Khalikov <eldorabdukhalikov74@gmail.com>
2026-04-28 22:26:40 +0500

    i edded image

--- src/seed.ts
+++ src/seed.ts
@@ -23,21 +23,21 @@ const seed = async () => {
                 style: ClothingStyle.CLASSIC,
                 price: 550000,
                 sizes: ["S", "M"],
-                image: "https://images.unsplash.com/photo-1539008835657-9e8e9680c956?q=80&w=400&h=600&fit=crop",
+                image: "AgACAgIAAxkBAAIBM2nw6WMNi-iCRCAGhvuqnGCy4exWAAKyF2sbFE2JS0SGrQ9ziTIlAQADAgADeQADOwQ",
             },
             {
                 name: "Casual Streetwear Pants",
                 style: ClothingStyle.CASUAL,
                 price: 280000,
                 sizes: ["S", "M", "L", "XL", "XXL"],
-                image: "https://images.unsplash.com/photo-1541099649105-f69ad21f3246?q=80&w=400&h=600&fit=crop",
+                image: "AgACAgIAAxkBAAIBM2nw6WMNi-iCRCAGhvuqnGCy4exWAAKyF2sbFE2JS0SGrQ9ziTIlAQADAgADeQADOwQ",
             },
             {
                 name: "Sporty Windbreaker",
                 style: ClothingStyle.SPORTY,
                 price: 420000,
                 sizes: ["L", "XL"],
-                image: "https://images.unsplash.com/photo-1506157786151-b8491531f063?q=80&w=400&h=600&fit=crop",
+                image: "AgACAgIAAxkBAAIBM2nw6WMNi-iCRCAGhvuqnGCy4exWAAKyF2sbFE2JS0SGrQ9ziTIlAQADAgADeQADOwQ",
             },
         ];
 
